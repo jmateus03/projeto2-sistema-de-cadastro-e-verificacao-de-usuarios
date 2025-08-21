a169edfb48504646f0edb6557165210a21ccf6bb
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-08-21 16:02:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 //Cadastro de usuario
 const nome = prompt("Digite seu nome:")
 let idade = parseInt(prompt("digite sua idade"))
-
-
 //Verifica a idade do usuario
 if(idade < 13) {
   alert("você não tem idade suficiente, não é possivel realizar o cadastro")
-} else if (idade >= 13) {
+}else if (idade >= 13) {
 //perguntar a senha
  let senha = prompt("Crie sua senha")
- alert("Cadastro feito com sucesso!")
-
 //Login
 let aprovNome = prompt("Digite seu nome novamente:")
 let aprovSenha = prompt("Digite sua senha novamente:")
-
 //Verifica se a senha e o nome estão corretos
-if(aprovNome === nome && aprovSenha === senha) {
+if(aprovNome === nome && aprovSenha === senha){
 alert(`Login realizado com sucesso! Bem-vindo, ${nome}.`)
 }else {
     alert(`Nome ou senha incorretos. Tente novamente.`)
